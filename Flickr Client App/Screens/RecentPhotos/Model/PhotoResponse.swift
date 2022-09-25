b338a21b15930504733b6c7bbdf3dfd18868970f
aliberkayberber <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>
2022-09-25 21:54:29 +0300

    Initial Commit

---
+++ Flickr Client App/Screens/RecentPhotos/Model/PhotoResponse.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoResponse.swift
+//  Flickr Client App
+//
+//  Created by Ali Berkay BERBER on 1.09.2022.
+//
+
+import Foundation
+
+struct PhotoResponse: Codable {
+    let photos: Photos?
+}
